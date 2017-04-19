cd729bb3de80066e5e9622dc40e948ce3a509141
Joaquin Lavera <jlavera@jhl.local>
2017-04-19 15:21:14 -0300

    Fix initial state of courses

--- src/js/components/course/index.js
+++ src/js/components/course/index.js
@@ -17,7 +17,7 @@ class Course extends Component {
     return (
       <div>
         <span className={this.isBlocked() ? 'blocked' : ''}><b>{this.props.course.code}</b> {this.props.course.name}</span>
-        <span className={this.props.course.state === 'N' ? 'bold' : ''} onClick={() => this.props.onChangeState('N', this.props.course.code)}> N </span>
+        <span className={!this.props.course.state || this.props.course.state === 'N' ? 'bold' : ''} onClick={() => this.props.onChangeState('N', this.props.course.code)}> N </span>
         <span className={this.props.course.state === 'S' ? 'bold' : ''} onClick={() => this.props.onChangeState('S', this.props.course.code)}> F </span>
         <span className={this.props.course.state === 'A' ? 'bold' : ''} onClick={() => this.props.onChangeState('A', this.props.course.code)}> A </span>
         {this.props.course.dependencies.map((dep, index) => <Dependency key={'' + dep.code + index} course={dep}/>)}
