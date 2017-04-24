1de9e9a2234ffe2651f92e00fa44fbd7d7a2ed4b
Joaquin Lavera <jlavera92@gmail.com>
2017-04-24 03:06:45 -0300

    Sort courses

--- src/js/components/year/index.js
+++ src/js/components/year/index.js
@@ -11,7 +11,7 @@ class Year extends Component {
     return (
       <div className='col-xs-2'>
         <div className="year-title h2">{this.props.year}◦ año:</div>
-        {this.props.courses.map(course =>
+        {orderedList(this.props.courses).map(course =>
           <Course key={course.code} course={course} onChangeState={this.props.onChangeState} />
         )}
       </div>
@@ -20,3 +20,31 @@ class Year extends Component {
 };
 
 export default Year;
+
+function orderedList(courses) {
+  return courses.sort((c1, c2) => {
+    if (c1.main) {
+      return -1;
+    }
+
+    if (c2.main) {
+      return 1;
+    }
+
+    if (c1.duration < c2.duration) {
+      return -1;
+    }
+
+    if (c1.duration > c2.duration) {
+      return 1;
+    }
+
+    if (c1.name < c2.name) {
+      return -1;
+    }
+
+    if (c1.name > c2.name) {
+      return 1;
+    }
+  });
+}
