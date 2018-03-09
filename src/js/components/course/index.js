ee9466431dc53417004ac22b0c2c07d00a60700d
Joaquin Lavera <jlavera@medallia.com>
2018-03-09 20:05:10 -0300

    redesign

--- src/js/components/course/index.js
+++ src/js/components/course/index.js
@@ -49,36 +49,12 @@ class Course extends Component {
   render() {
     return (
       <div className={(this.isBlocked() ? 'panel-warning' : this.isApproved() ? 'panel-success' : 'panel-info') + " panel"}>
-        <div className="panel-heading">
-          <div className={(this.props.course.main ? "underlined" : "" ) + " course-name panel-title"}>{this.props.course.name}</div>
-
-          {this.hasDeppendencies() ? <div aria-controls="collapseExample" aria-expanded="true" className="course-arrow glyphicon glyphicon-chevron-down" data-target={"#deps-" + this.props.course.code} data-toggle="collapse" type="button"></div> : ''}
-
-          <div className="separator"></div>
-        </div>
-        <div className="panel-body">
-          <div className="course-code col-xs-4">     {this.props.course.code}      </div>
-          <div className="course-duration col-xs-4"> {this.props.course.duration}  </div>
-          <div className="course-hours col-xs-4">    {this.props.course.hours}Hrs. </div>
-
-          <div className="separator"></div>
-
-
-          <div id={"deps-" + this.props.course.code} className="collapse">
-            {this.hasDepsToSign() ? <hr className="course-separator" /> : ''}
-
-            {this.props.course.dependencies.toSign.filter(dep => dep.type === 'S').map((dep, index) => <Dependency key={'' + dep.course.code + index} course={dep}/>)}
-
-            {this.hasDepsToApprove() ? <hr className="course-separator" /> : ''}
-
-            {this.props.course.dependencies.toApprove.filter(dep => dep.type === 'A').map((dep, index) => <Dependency key={'' + dep.course.code + index} course={dep}/>)}
-          </div>
-        </div>
-        <div className="panel-footer">
-          <div className={(this.isNone()     ? 'active' : '') + " btn btn-default btn-xs col-xs-4"} onClick={() => this.props.onChangeState('N', this.props.course.code)}> N </div>
-          <div className={(this.isSigned()   ? 'active' : '') + " btn btn-default btn-xs col-xs-4"} onClick={() => this.props.onChangeState('S', this.props.course.code)}> F </div>
-          <div className={(this.isApproved() ? 'active' : '') + " btn btn-default btn-xs col-xs-4"} onClick={() => this.props.onChangeState('A', this.props.course.code)}> A </div>
-          <div className="separator"></div>
+        <div className="course-grid-container">
+          <div className="course-grid-item-course-name">{this.props.course.name}</div>
+          <div className={'course-grid-item-course-button ' + (this.isNone()     ? 'active' : '') + " btn btn-default btn-xs"} onClick={() => this.props.onChangeState('N', this.props.course.code)}>Pendiente</div>
+          <div className={'course-grid-item-course-button ' + (this.isSigned()   ? 'active' : '') + " btn btn-default btn-xs"} onClick={() => this.props.onChangeState('S', this.props.course.code)}>Firmada</div>
+          <div className="course-grid-item-course-status">Bloqueada</div>
+          <div className={'course-grid-item-course-button ' + (this.isApproved() ? 'active' : '') + " btn btn-default btn-xs"} onClick={() => this.props.onChangeState('A', this.props.course.code)}>Aprobada</div>
         </div>
       </div>
     );
