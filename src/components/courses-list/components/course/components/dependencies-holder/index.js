5f6444586d9096ad8fb964f77d45992f6766a8a9
Joaquin Lavera <jlavera@medallia.com>
2018-09-08 01:40:49 -0300

    Fix propType

--- src/components/courses-list/components/course/components/dependencies-holder/index.js
+++ src/components/courses-list/components/course/components/dependencies-holder/index.js
@@ -2,28 +2,31 @@ import React from 'react'
 import { arrayOf, string, shape } from 'prop-types';
 import './style.css';
 
-const DependenciesHolder = props => (
-  <div className='dependencies-holder'>
-    <div className='circle green'></div>
-    <div className='dependencies-holder-text'>
-      { props.text } 1/{ props.signed.length + props.approved.length }
+const DependenciesHolder = props => {
+  const toSign = props.requiredState === 'S';
+
+  return (
+    <div className='dependencies-holder'>
+      <div className='circle green'></div>
+      <div className='dependencies-holder-text'>
+        { props.text }
+        1 /{ props.signed.length + props.approved.length }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 DependenciesHolder.propTypes = {
   text: string.isRequired,
   requiredState: string.isRequired,
-  dependencies: shape({
-    approved: arrayOf(shape({
-      name: string.isRequired,
-      code: string.isRequired
-    })),
-    signed: arrayOf(shape({
-      name: string.isRequired,
-      code: string.isRequired
-    }))
-  })
+  approved: arrayOf(shape({
+    name: string.isRequired,
+    code: string.isRequired
+  })),
+  signed: arrayOf(shape({
+    name: string.isRequired,
+    code: string.isRequired
+  }))
 };
 
 export default DependenciesHolder;
