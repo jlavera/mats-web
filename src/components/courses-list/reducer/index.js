38d54029c4c2168f57f62debec12c7d523359ad8
Joaquin Lavera <jlavera@medallia.com>
2018-09-15 20:51:22 -0300

    Leo get triggered

--- src/components/courses-list/reducer/index.js
+++ src/components/courses-list/reducer/index.js
@@ -9,14 +9,21 @@ import {
   UPDATESUCCESS
 } from '../actions';
 
+import {
+  UPDATE_PREVIEW_MODE_ENABLED
+} from '../../top-bar/actions';
+
 const initialState = {
-  fixture:    [],
-  isFetching: false,
-  error:      null,
+  fixture:     [],
+  isFetching:  false,
+  error:       null,
+  previewMode: false
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case UPDATE_PREVIEW_MODE_ENABLED:
+      return { ...state, previewMode: action.payload.state };
     case COURSESLIST_REQUEST:
       return { ...state, isFetching: true, error: null, fixture: [] };
     case COURSESLIST_SUCCESS:
@@ -31,7 +38,9 @@ export default function (state = initialState, action) {
       const rest = reject(propEq('code', action.payload.code))(state.fixture);
       const updatedCourse = { ...course, state: action.payload.state };
 
-      stateStorage.set(updatedCourse);
+      if (!state.previewMode) {
+        stateStorage.set(updatedCourse);
+      }
 
       return { ...state, fixture: [ updatedCourse, ...rest ] };
     case UPDATESUCCESS:
