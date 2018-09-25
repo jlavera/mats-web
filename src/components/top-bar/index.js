3860a154dd4dbe61f7f6b628b6ce2cd77fa955ee
Joaquin Lavera <jlavera@medallia.com>
2018-09-25 01:44:14 -0300

    tunning preview

--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect }          from 'react-redux';
 import { withRouter }       from 'react-router';
+import ReactTooltip         from 'react-tooltip';
 import {
   isPreviewModeEnabledSelector
 } from '../../shared/selectors';
 import { compose } from 'ramda';
+import cx from 'classnames';
 
 import { doUpdatePreviewModeEnabled } from '../../shared/actions';
 
@@ -12,13 +14,28 @@ import './style.css';
 
 class TopBar extends Component {
   render() {
+    const {
+      doUpdatePreviewModeEnabled,
+      previewMode
+    } = this.props;
+
+    const getClassName = active => cx(
+      'top-bar-text',
+      active ? 'active' : 'inactive'
+    );
+
     return (
       <div id='top-bar'>
+        <div className='top-bar-text'>Modo borrador: </div>
+        <div className={getClassName(!previewMode)}>Descativado</div>
         <label id='top-bar-switch'>
-          <input id='top-bar-input' type='checkbox' onClick={() => this.props.doUpdatePreviewModeEnabled(!this.props.previewMode)}/>
+          <input id='top-bar-input' type='checkbox' onClick={() => doUpdatePreviewModeEnabled(!previewMode)}/>
           <span id='top-bar-slider'></span>
         </label>
-        <div id='top-bar-text'>Modo borrador (Los cambios no serán guardados)</div>
+        <div className={getClassName(previewMode)}>Activado <i data-tip data-for='preview-info-tooltip' className='preview-info fa fa-question-circle'></i></div>
+        <ReactTooltip id='preview-info-tooltip' type='dark'>
+          <span>Los cambios en modo borrador no se guardarán y al desactivarlo se volverá al último estado guardado.</span>
+        </ReactTooltip>
       </div>
     );
   }
